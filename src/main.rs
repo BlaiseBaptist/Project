e9c1b42c516a7a35cd849610767b0e4f81b3088f
BlaiseBaptist <bbaptist@thegraysonschool.org>
2024-10-10 14:19:40 -0400

    improved graphing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 #[derive(Default)]
 struct Counter {
-    value: i64,
+    value: usize,
 }
 #[derive(Debug, Clone, Copy)]
 enum Message {
-    ValueChange(f64),
-    NewValue(f64),
+    ValueChange(isize),
+    NewValue(usize),
 }
 
 use iced::widget::{button, canvas, column, row, slider, text, Column};
@@ -13,27 +13,33 @@ use iced::{mouse, Color, Rectangle, Renderer, Theme};
 impl Counter {
     fn view(&self) -> Column<Message> {
         column![
-            /*text(self.value).size(100),
+            text(self.value).size(100),	
             row![
-                button("+").on_press(Message::ValueChange(1.0)),
-                button("*").on_press(Message::ValueChange(self.value as f64)),
+                button("+").on_press(Message::ValueChange(10)),
+//                button("*").on_press(Message::ValueChange(self.value as f64)),
             ],
             row![
-                button("-").on_press(Message::ValueChange(-1.0)),
-                button("/").on_press(Message::ValueChange((self.value as f64) * -0.5)),
+                button("-").on_press(Message::ValueChange(-10)),
+//                button("/").on_press(Message::ValueChange((self.value as f64) * -0.5)),
             ],
+			/*
             slider(
                 0.0..=(self.value as f64) * 1.5 + 10.0,
                 self.value as f64,
                 Message::NewValue
        		),*/
-			canvas(Graph::new((1..100).map(|x| x as f32).collect())),
+			if self.value >= 10{
+				canvas(Graph::new(function(self.value )))
+			}
+			else{
+				canvas(Graph::new(function(10)))
+			}
         ]
     }
     fn update(&mut self, message: Message) {
         match message {
-            Message::ValueChange(v) => self.value += v.round() as i64,
-            Message::NewValue(v) => self.value = v.round() as i64,
+            Message::ValueChange(v) => self.value = self.value.overflowing_add_signed(v).0,
+            Message::NewValue(v) => self.value = v,
         }
     }
 }
@@ -42,15 +48,17 @@ struct Graph {
     x_size: usize,
     y_size: usize,
     values: Vec<f32>,
-	scale: f32,
+	x_scale: f32,
+	y_scale: f32,
 }
 impl Graph {
     fn new(values: Vec<f32>) -> Graph {
         Graph {
-            x_size: values.len()*10,
-            y_size: values.clone().into_iter().reduce(f32::max).unwrap() as usize * 10,
+            x_size: values.len(),
+            y_size: values.clone().into_iter().reduce(f32::max).unwrap() as usize * 40,
             values: values,
-			scale: 10.0,
+			x_scale: 1.0,
+			y_scale: 10.0
         }
     }
 }
@@ -69,26 +77,29 @@ impl<Message> canvas::Program<Message> for Graph {
             iced::Size::new(self.x_size as f32, self.y_size as f32),
         );
         let style = canvas::Stroke {
-            line_cap: canvas::LineCap::Butt,
+            line_cap: canvas::LineCap::Round,
             line_dash: canvas::LineDash {
                 offset: 0,
                 segments: &[1.0,0.0],
             },
-            line_join: canvas::LineJoin::Round,
-            width: 10.0,
+            line_join: canvas::LineJoin::Bevel,
+            width: 5.0,
             style: canvas::Style::Solid(Color::BLACK),
         };
         for i in 1..self.values.len() {
             frame.stroke(
                 &canvas::Path::line(
-                    iced::Point::new((i - 1) as f32 * self.scale, self.values[i - 1] * self.scale),
-                    iced::Point::new((i) as f32 * self.scale, self.values[i] * self.scale),
+                    iced::Point::new((i - 1) as f32 * self.x_scale, self.values[i - 1] * self.y_scale),
+                    iced::Point::new((i) as f32 * self.x_scale, self.values[i] * self.y_scale),
                 ),
                 style,
             );
     	}    
         vec![frame.into_geometry()]
     }
+}
+fn function(x_size: usize) -> Vec<f32>{
+	(0..x_size).map(|x| ((x as f32 * std::f32::consts::PI * 0.05).sin() + 1.0) ).collect()
 }
 fn main() {
     let _ = iced::run("A cool counter", Counter::update, Counter::view);
