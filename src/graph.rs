c18bbcae93b4344b8e60c20ad4227c7bb52b0240
BlaiseBaptist <blaise.baptist@gmail.com>
2024-12-13 12:55:51 -0500

    Mouse controls for zooming and panning

--- src/graph.rs
+++ src/graph.rs
@@ -4,7 +4,7 @@ pub mod graph {
         mouse,
         widget::canvas,
         widget::canvas::{event, Event},
-        Rectangle, Renderer, Theme,
+        Point, Rectangle, Renderer, Theme,
     };
     #[derive(Debug)]
     pub struct Graph {
@@ -23,7 +23,7 @@ pub mod graph {
         }
     }
     impl<Message> canvas::Program<Message> for Graph {
-        type State = [f32; 2];
+        type State = GraphControls;
         fn draw(
             &self,
             state: &Self::State,
@@ -34,31 +34,31 @@ pub mod graph {
         ) -> Vec<canvas::Geometry> {
             let mut frame = canvas::Frame::new(renderer, bounds.size());
             let len = self.values.len();
-            let start = match len.checked_sub((bounds.size().width / state[0].abs()) as usize) {
+            let start = match len.checked_sub((bounds.size().width * state.x_scale) as usize) {
                 Some(v) => v,
                 _ => 0,
             };
             let scale = canvas::path::lyon_path::geom::euclid::Transform2D::new(
-                state[0].abs(),
+                1.0 / state.x_scale,
                 0.0,
                 0.0,
-                -bounds.size().height / state[1],
-                0.0,
-                bounds.size().height - 10.0,
+                -bounds.size().height / state.y_scale,
+                state.x_shift - 1.0,
+                bounds.size().height + state.y_shift - 10.0,
             );
             let height = (-scale.m32 / scale.m22) as u32;
             let mut lines = canvas::path::Builder::new();
             self.values
                 .iter()
-                .skip(start)
+                .take(start)
                 .enumerate()
                 .for_each(|(i, value)| {
-                    lines.line_to(iced::Point::new(
+                    lines.line_to(Point::new(
                         i as f32,
                         if value < &height {
                             *value as f32
                         } else {
-                            height as f32
+                            height as f32 + 10.0
                         },
                     ))
                 });
@@ -80,25 +80,69 @@ pub mod graph {
             state: &mut Self::State,
             event: Event,
             bounds: Rectangle,
-            _cursor: mouse::Cursor,
+            cursor: mouse::Cursor,
         ) -> (event::Status, Option<Message>) {
-            if state[0] < bounds.size().width / (10.0 * self.values.len() as f32) {
-                state[0] = bounds.size().width / (10.0 * self.values.len() as f32);
-            }
+            let mut event_status = event::Status::Ignored;
             match event {
                 Event::Mouse(e) => match e {
                     mouse::Event::WheelScrolled {
                         delta: mouse::ScrollDelta::Pixels { x, y },
                     } => {
-                        state[0] += x / 1000.0;
-                        state[1] += y;
-                        return (event::Status::Captured, None);
+                        state.x_scale += x / 100.0;
+                        state.y_scale += y;
+                        event_status = event::Status::Captured;
                     }
+                    mouse::Event::ButtonPressed(mouse::Button::Left) => {
+                        state.last_mouse_click = (|| -> Option<Point> {
+                            Some(
+                                cursor.position()?
+                                    + iced::Vector::new(-state.x_shift, -state.y_shift),
+                            )
+                        })();
+                    }
+                    mouse::Event::ButtonReleased(mouse::Button::Left) => {
+                        state.last_mouse_click = None
+                    }
+                    mouse::Event::CursorMoved { position } => match state.last_mouse_click {
+                        Some(last_position) => {
+                            state.x_shift = position.x - last_position.x;
+                            state.y_shift = position.y - last_position.y;
+                        }
+                        None => {}
+                    },
                     _ => {}
                 },
                 _ => {}
             };
-            (event::Status::Ignored, None)
+            if state.x_scale < 2.0 {
+                state.x_scale = 2.0;
+            }
+            if state.x_scale > self.values.len() as f32 / (bounds.size().width * 2.0) {
+                state.x_scale = self.values.len() as f32 / (bounds.size().width * 2.0);
+            }
+            if state.y_scale < 2.0 {
+                state.y_scale = 2.0;
+            }
+            (event_status, None)
+        }
+    }
+    #[derive(Debug)]
+    pub struct GraphControls {
+        x_scale: f32,
+        y_scale: f32,
+        x_shift: f32,
+        y_shift: f32,
+        last_mouse_click: Option<Point>,
+    }
+    impl std::default::Default for GraphControls {
+        fn default() -> GraphControls {
+            GraphControls {
+                x_scale: 1.0,
+                y_scale: 1.0,
+                x_shift: 0.0,
+                y_shift: 0.0,
+                last_mouse_click: None,
+            }
         }
     }
 }
